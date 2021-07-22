2817f5beb50ba37d63c4acec2a132fa4b88dbc2d
Abhishek Kumar <abhishek1542512@gmail.com>
2021-07-22 15:05:43 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const signUpButton = document.getElementById("btn_clc");
+const signInButton = document.getElementById("user_name");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
